07fee916ac8b3d39aac0ca4419d3c44ab38f99d9
Tejaswi SK <32892292+Tejas163@users.noreply.github.com>
2024-08-12 14:55:13 +0530

    updated to remove dashes and replaced with underscore

--- scripts/run-feature-and-prediction-pipelines.sh
+++ scripts/run-feature-and-prediction-pipelines.sh
@@ -3,6 +3,6 @@
 
 set -e
 
-jupyter nbconvert --to notebook --execute iris-features-pipeline.ipynb
-jupyter nbconvert --to notebook --execute iris-batch-inference-pipeline.ipynb
+jupyter nbconvert --to notebook --execute iris_features_pipeline.ipynb
+jupyter nbconvert --to notebook --execute iris_batch_inference_pipeline.ipynb
 
